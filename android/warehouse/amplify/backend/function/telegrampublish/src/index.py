b56418fecfe64dafc64d42d10e0a95a391fec0b8
Boris Resnick <boris@resnick.ru>
2021-02-21 18:56:57 +0300

    Added telegram layer

--- android/warehouse/amplify/backend/function/telegrampublish/src/index.py
+++ android/warehouse/amplify/backend/function/telegrampublish/src/index.py
@@ -1,25 +1,33 @@
 import json
-import requests
 
-BOT_API_KEY = "1604602953:AAFoaCe-CCSqbnX61zVcVIbg-I4bGgFAX8k"
-CHAT_ID = "-1001202477347"
+from telegram import Bot
+from telegram.utils import helpers
 
-def handler(event, context):
+BOT_API_KEY = "1604602953:AAFoaCe-CCSqbnX61zVcVIbg-I4bGgFAX8k"  # Bargain Cave Bot
+CHANNEL_ID = '-1001202477347'   # Bargain Cave Staging
+
+LOT_ID = '2f96753c-70e0-4711-a984-7d9fe86cba14'
+LOT_PHOTO = 'https://bargaincave-photos142503-staging.s3.eu-west-2.amazonaws.com/public/lot_photo_1307507264875693089.jpg'
+
+# Parameters: event, context
+def handler(event, _):
     body = json.loads(event['body'])
     content = body['bot_message_contents']
 
     print('built content:')
     print(content)
 
-    url = f"https://api.telegram.org/bot{BOT_API_KEY}/sendMessage?chat_id={CHAT_ID}&text={content}"
-
-    response = requests.post(url).json()
-
-    print("response content:")
-    print(response)
+    bot = Bot(BOT_API_KEY)
+    deep_url = helpers.create_deep_linked_url(bot.username, LOT_ID)
+    caption = f'{content}: {deep_url}'
+    message = bot.send_photo(
+        chat_id=CHANNEL_ID,
+        photo=LOT_PHOTO,
+        caption=caption
+    )
 
     return_code = 503
-    if response['ok']:
+    if message:
         return_code = 200
 
     return {
