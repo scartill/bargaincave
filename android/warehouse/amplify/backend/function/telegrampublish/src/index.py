faf425fc2e86b8ad335bb79ccc287198673e49fa
Boris Resnick <boris@resnick.ru>
2021-02-22 22:16:08 +0300

    Moved bot api token to secret manager

--- android/warehouse/amplify/backend/function/telegrampublish/src/index.py
+++ android/warehouse/amplify/backend/function/telegrampublish/src/index.py
@@ -1,34 +1,46 @@
+import os
 import json
+
+import boto3
 
 from telegram import Bot
 from telegram.utils import helpers
 
-BOT_API_KEY = "1604602953:AAFoaCe-CCSqbnX61zVcVIbg-I4bGgFAX8k"  # Bargain Cave Bot
-CHANNEL_ID = '-1001202477347'   # Bargain Cave Staging
+TELEGRAM_BOT_TOKEN_SECRET_NAME = "telegram_token"
 
 LOT_ID = '2f96753c-70e0-4711-a984-7d9fe86cba14'
 LOT_PHOTO = 'https://bargaincave-photos142503-staging.s3.eu-west-2.amazonaws.com/public/lot_photo_1307507264875693089.jpg'
 
 # Parameters: event, context
 def handler(event, _):
-    body = json.loads(event['body'])
-    content = body['bot_message_contents']
-
-    print('built content:')
-    print(content)
-
-    bot = Bot(BOT_API_KEY)
-    deep_url = helpers.create_deep_linked_url(bot.username, LOT_ID)
-    caption = f'{content}: {deep_url}'
-    message = bot.send_photo(
-        chat_id=CHANNEL_ID,
-        photo=LOT_PHOTO,
-        caption=caption
-    )
-
     return_code = 503
-    if message:
-        return_code = 200
+
+    try:
+        body = json.loads(event['body'])
+        content = body['bot_message_contents']
+
+        sm = boto3.client('secretsmanager')
+        secret_value_response = sm.get_secret_value(
+            SecretId=TELEGRAM_BOT_TOKEN_SECRET_NAME
+        )
+        bot_token = secret_value_response['SecretString']
+
+        channel_id = os.environ['CHANNEL_ID']
+
+        bot = Bot(bot_token)
+        deep_url = helpers.create_deep_linked_url(bot.username, LOT_ID)
+        caption = f'{content}: {deep_url}'
+        message = bot.send_photo(
+            chat_id=channel_id,
+            photo=LOT_PHOTO,
+            caption=caption
+        )
+
+        if message:
+            return_code = 200
+    except Exception as e:
+        print(f'Telegram exception {e}')
+
 
     return {
         'statusCode': return_code,
