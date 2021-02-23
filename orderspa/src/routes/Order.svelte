6eae7f3b10035010b73dc672c7a9d8d502afdcba
Boris Resnick <boris@resnick.ru>
2021-02-23 03:14:08 +0300

    Order from @ Svelte

--- orderspa/src/routes/Order.svelte
+++ orderspa/src/routes/Order.svelte
@@ -1,22 +1,25 @@
 <script>
     export let params = {};
+    let name = ''
+    let phone = ''
 
     function submitClick() {
-        alert('Submitting')
+        alert('Submitting ' + params.lot_id + ' ' + name + ' ' + phone) 
     }
 </script>
 
 <h1>Bargain Cave Order</h1>
-<p>Lot ID: {params.lot_id}</p>
-
-<button on:click={submitClick}>Submit</button>
+<div>
+    <p>Name: <input bind:value={name}/>
+    <p>Phone: <input bind:value={phone}/></p>
+    <button on:click={submitClick}>Submit</button>
+</div>
 
 <style>
 	h1 {
 		color: #ff00ffff;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 2em;
 		font-weight: 100;
 	}
 </style>
-
