5114cfdac68508f11632ef12c0ceb12625a40e6f
Boris Resnick <boris@resnick.ru>
2021-02-24 14:24:10 +0300

    Order page done

--- orderspa/src/main.js
+++ orderspa/src/main.js
@@ -5,7 +5,7 @@ import Amplify, { API } from 'aws-amplify';
 // TODO: generate aws-exports using Amplify Headless Mode
 import aws_exports from './aws-exports';
 
-Amplify.configure(aws_exports);
+Amplify.API.configure(aws_exports);
 
 const app = new App({
 	target: document.body
--- orderspa/src/routes/Order.svelte
+++ orderspa/src/routes/Order.svelte
@@ -2,11 +2,13 @@
     import Amplify, { API } from 'aws-amplify';
 
     export let params = {};
-    let name = ''
-    let phone = ''
+    let status = 'init';
+
+    let name = '';
+    let phone = '';
 
     function submitClick() {
-        console.log('submit', params.lot_id, name, phone) 
+        status = 'submitting'
 
         const apiName = 'warehouseapi';
         const path = '/crm/deal/create';
@@ -22,20 +24,44 @@
             .post(apiName, path, post_init)
             .then(response => {
                 console.log('API call success', response)
+                status = 'success'
             })
             .catch(error => {
                 console.log(error.response);
+                status = 'error'
             });
     }
 </script>
 
 <h1>Bargain Cave Order</h1>
-<div>
-    <p>Name: <input bind:value={name}/>
-    <p>Phone: <input bind:value={phone}/></p>
-    <button on:click={submitClick}>Submit</button>
-    <p>&copy; Bargain Cave, 2021</p>
-</div>
+
+{#if status == 'init'}
+    <div>
+        <p>Name: <input bind:value={name}/>
+        <p>Phone: <input bind:value={phone}/></p>
+        <button on:click={submitClick}>Submit</button>
+    </div>
+{/if}
+
+{#if status == 'submitting'}
+    <div>
+        <p>Submitting, please wait...</p>
+    </div>
+{/if}
+
+{#if status == 'success'}
+    <div>
+        <p>Thank you for your order!</p>
+    </div>
+{/if}
+
+{#if status == 'error'}
+    <div>
+        <p>System error. We are sorry for inconvenience.</p>
+    </div>
+{/if}
+
+<p>&copy; Bargain Cave, 2021</p>
 
 <style>
 	h1 {
