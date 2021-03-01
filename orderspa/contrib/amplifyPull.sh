9f345767bcd82b4b4fdfd5a22918f305c6fc2d08
Boris Resnick <boris@resnick.ru>
2021-03-01 12:09:00 +0300

    CI debug

--- orderspa/contrib/amplifyPull.sh
+++ orderspa/contrib/amplifyPull.sh
@@ -10,7 +10,12 @@ SVELTECONFIG="{\
 }"
 
 AWSCLOUDFORMATIONCONFIG="{\
-\"configLevel\":\"general\"
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
+\"profileName\":\"default\",\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"$AWS_REGION\"\
 }"
 
 AMPLIFY="{\
